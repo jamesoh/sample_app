e0c5f68440b17d3cc9baa684077a3d14da38ed7b
jamesoh <joh@truecar.com>
2014-07-08 13:51:55 -0700

    Finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
@@ -8,7 +9,6 @@ SampleApp::Application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
-  get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "James Oh"
+    email     "joh@example.com"
+    password  "Frisco23!"
+    password_confirmation "Frisco23!"
+  end
+end
